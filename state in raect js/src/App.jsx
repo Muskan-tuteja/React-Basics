6cff3f19d4f06378422bb460a82204aadb171541
muskan <muskantuteja714@gmail.com>
2025-09-24 10:58:49 +0530

    24/9/25

---
+++ state in raect js/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  let firstName = "Muskan123"
+  
+
+  return (
+    <>
+    <h1>State in React Js</h1>
+    <h1>{firstName}</h1>
+    <button></button>
+      
+    </>
+  )
+}
+
+export default App
