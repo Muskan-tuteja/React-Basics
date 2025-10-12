0a7ee22b29b80c4e89ab68c40699f6b560d53bee
muskan <muskantuteja714@gmail.com>
2025-10-12 16:43:43 +0530

    12/10/25

---
+++ external style/framework bootstrap/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { Button } from 'react-bootstrap';
+
+function App() {
+ 
+
+  return (
+    <>
+    <Button>ok</Button>
+    <Button variant="danger"onClick={() =>alert("aler is submit")}>Click ME!</Button>
+      
+    </>
+  )
+}
+
+export default App
