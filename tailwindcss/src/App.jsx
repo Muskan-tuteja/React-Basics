9883a21fcb3c041a0b024cda25f16575bc0f0440
muskan <muskantuteja714@gmail.com>
2025-10-21 19:25:53 +0530

    tailwind css

---
+++ tailwindcss/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline text-amber-700 bg-blue-800'>Test styling </h1>
+    </>
+  )
+}
+
+export default App
