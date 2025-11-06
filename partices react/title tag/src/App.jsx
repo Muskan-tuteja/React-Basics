753c38c32b6403fa28694cf053fc45b28bb66f9c
muskan <muskantuteja714@gmail.com>
2025-11-06 18:30:16 +0530

    06/11/25

---
+++ partices react/title tag/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>React componets is used to title tag yes</h1>
+      <title>abdc</title>
+    </>
+  )
+}
+
+export default App
